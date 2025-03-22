c80887d8ff3c1252d53b087800827b0c76e7cc11
nono303 <git@nono303.net>
2025-03-22 17:03:05 +0100

    fix and add tests

--- tests/ExternalElevation/testElevationExternal.php
+++ tests/ExternalElevation/testElevationExternal.php
@@ -6,30 +6,29 @@
 	$gpx::$APPLY_ELEVATION_SMOOTHING = true;
 	$gpx::$ELEVATION_SMOOTHING_THRESHOLD = 2;
 	$gpx::$ELEVATION_EXTERNAL = true;
-	$gpx::$DEBUG = true;
+	$gpx::$DEBUG = phpGPX::LOG_INFO;
 
-	foreach([
-		"./gpx/2024-08-21 080359__20240821_0803.gpx",
-		"./gpx/2024-08-21 080359__20240821_0803-noalt.gpx",
-		"./gpx/rte.gpx",
-		"./gpx/rte-noalt.gpx",
-		"./gpx/ign.gpx",
-		"./gpx/ign-noalt.gpx",
-		"./gpx/2024-08-21-08-03-53.fit_reduced.gpx",
-	] as $gpxfile){
-		echo ">>>>>>>> loading ".realpath($gpxfile).PHP_EOL;
-		$file = $gpx->load($gpxfile);
-		echo "###################### METADATA".PHP_EOL.print_r($file->metadata,true);
-		echo "###################### CREATOR".PHP_EOL.print_r($file->creator,true);
-		echo PHP_EOL."###################### EXTENSIONS".PHP_EOL.print_r($file->extensions,true);
-		foreach([$file->tracks, $file->routes] as $statspointer){
-			$t = 0;
-			foreach($statspointer as $trkrte){
-				$stats = $trkrte->stats->toArray();
-				echo "######## ".get_class($trkrte)."[".($t++)."]".PHP_EOL;
-				foreach(["minAltitude","maxAltitude"] as $d)
-					echo "\t".$d.": ".$stats[$d].PHP_EOL;
+	foreach(glob("./gpx/*.gpx") as $gpxfile){
+		try{
+			$pb = microtime(true);
+			$file = $gpx->load($gpxfile);
+			echo ">>>>>>>> loading ".realpath($gpxfile)." ".PHP_EOL.(round((microtime(true)-$pb)*1000))."ms".PHP_EOL;
+			// echo "###################### METADATA".PHP_EOL.print_r($file->metadata,true);
+			// echo "###################### CREATOR".PHP_EOL.print_r($file->creator,true);
+			// echo PHP_EOL."###################### EXTENSIONS".PHP_EOL.print_r($file->extensions,true);
+			foreach([$file->tracks, $file->routes] as $statspointer){
+				$t = 0;
+				foreach($statspointer as $trkrte){
+					$stats = $trkrte->stats->toArray();
+					// echo PHP_EOL."###################### STATS".PHP_EOL.print_r($stats,true);
+					// echo "######## ".get_class($trkrte)."[".($t++)."]".PHP_EOL;
+					foreach(["minAltitude","maxAltitude","cumulativeElevationGain","cumulativeElevationLoss"] as $d)
+						echo "\t".$d.": ".$stats[$d].PHP_EOL;
+				}
 			}
+			file_put_contents("./res/".basename($gpxfile),$file->toXML()->saveXML());
+		} catch(Exception $e){
+			echo "!!!!!!!! EXCEPTION ".realpath($gpxfile).": ".$e->getMessage().PHP_EOL;//.$e->getTraceAsString().PHP_EOL;
 		}
 	}
 ?>
